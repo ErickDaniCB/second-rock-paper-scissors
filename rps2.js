6e81eba71d52bc1db39752501f0836d03ea25d27
Erick CB <erickdani10@hotmail.com>
2024-09-17 13:30:52 -0600

    Add function to transform random number to literal play

--- rps2.js
+++ rps2.js
@@ -1,8 +1,26 @@
-function computerGuess() {
+function randomNumber02() {
   let random = Math.floor(Math.random() * 3);
-  return random
+  return random;
 }
 
+// Loop for testing randomness
+
 // for (let i = 0; i < 10; i++) {
-//     console.log(computerGuess());
+//     console.log(numberToPlay());
 // }
+
+function numberToPlay(number = randomNumber02()) {
+  let play;
+  switch (number) {
+    case 0:
+      play = "rock";
+      break;
+    case 1:
+      play = "paper";
+      break;
+    case 2:
+      play = "scissors";
+      break;
+  }
+  return play;
+}
